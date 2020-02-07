6aac7419dc7f28ba2615abe8e4cd4a63e7f1f0ac
Milad <milad.izadkhast@edu-kw1c.nl>
2020-02-07 14:03:52 +0100

    Tamplate allemaal af, Opdracht_2.1.php is ook af

---
+++ hoofdstuk2/Opdracht_2.1.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * User: milad
+ * Date: 7-2-2020
+ * Time: 13:55
+ * File: opdracht_2.1.php
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+
+    </title>
+
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <meta charset="utf-8">
+    <link href="../CSS/style.css" type="text/css" rel="stylesheet">
+
+</head>
+<body>
+<h2>Uitwerking van <a href="../Index.php"> PHP-opdrachten</a></h2>
+<div class="box">
+    <?php
+    echo ("Hello World!")
+    ?>
+</div>
+</body>
+</html>
