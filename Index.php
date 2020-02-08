6f3a3a3454f3a8ea7be4e73819f995e288a79634
Milad <milad.izadkhast@edu-kw1c.nl>
2020-02-08 14:35:12 +0100

    Alle linkjes naar bestanden toegevoegd en style.css een beetje veranderd

--- Index.php
+++ Index.php
@@ -35,26 +35,25 @@
                 </li>
                 <li>Hoofdstuk 3
                     <ul>
-                        <li><a href="">Opdracht 3.1</a></li>
-                        
-                        <li><a href="">Opdracht 3.2</a></li>
-                        <li><a href="">Opdracht 3.3</a></li>
+                        <li><a href="hoofdstuk3/Opdracht_3.1.php">Opdracht 3.1</a></li>
+                        <li><a href="hoofdstuk3/Opdracht_3.2.php">Opdracht 3.2</a></li>
+                        <li><a href="hoofdstuk3/Opdracht_3.3.php">Opdracht 3.3</a></li>
                     </ul>
                 </li>
                 <li>Hoofdstuk 4
                     <ul>
-                        <li><a href="">Opdracht 4.1</a></li>
-                        <li><a href="">Opdracht 4.2</a></li>
-                        <li><a href="">Opdracht 4.3</a></li>
-                        <li><a href="">Opdracht 4.4</a></li>
+                        <li><a href="hoofdstuk4/Opdracht_4.1.php">Opdracht 4.1</a></li>
+                        <li><a href="hoofdstuk4/Opdracht_4.2.php">Opdracht 4.2</a></li>
+                        <li><a href="hoofdstuk4/Opdracht_4.3.php">Opdracht 4.3</a></li>
+                        <li><a href="hoofdstuk4/Opdracht_4.4.php">Opdracht 4.4</a></li>
                     </ul>
                 </li>
                 <li>Hoofdstuk 5
                     <ul>
-                        <li><a href="">Opdracht 5.1</a></li>
-                        <li><a href="">Opdracht 5.2</a></li>
-                        <li><a href="">Opdracht 5.3</a></li>
-                        <li><a href="">Opdracht 5.4</a></li>
+                        <li><a href="hoofdstuk5/Opdracht_5.1.php">Opdracht 5.1</a></li>
+                        <li><a href="hoofdstuk5/Opdracht_5.2.php">Opdracht 5.2</a></li>
+                        <li><a href="hoofdstuk5/Opdracht_5.3.php">Opdracht 5.3</a></li>
+                        <li><a href="hoofdstuk5/Opdracht_5.4.php">Opdracht 5.4</a></li>
                     </ul>
                 </li>
             </ul>
