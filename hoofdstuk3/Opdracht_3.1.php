a7575bd2a5b9206bd97dd58783c46c4311d9fe60
Milad <milad.izadkhast@edu-kw1c.nl>
2020-02-21 12:36:00 +0100

    Opdracht_3.1.php veranderd

--- hoofdstuk3/Opdracht_3.1.php
+++ hoofdstuk3/Opdracht_3.1.php
@@ -24,30 +24,29 @@
 <h2>Uitwerking van <a href="../Index.php"> PHP-opdrachten</a></h2>
 <div class="box">
     <?php
-    $elf = "<u>Elfstedentocht</u>";
-    $alve = "<u>Alvestêdetocht</u>";
+    $soorttocht = "<u>Elfstedentocht</u>";
+    $tocht_fr = "<u>Alvestêdetocht</u>";
     $km = "<u>200</u>";
-    $schaats = "<u>schaatstocht</u>";
-    $natuur = "<u>natuurijs</u>";
-    $kvdfes = "<u>Koninklijke Vereniging De Friesche Elf Steden</u>";
-    $leeuw = "<u>Leeuwarden</u>";
-    $fries = "<u>Friesland</u>";
-    $num = "<u>15</u>";
-
-
+    $tocht = "<u>schaatstocht</u>";
+    $type = "<u>natuurijs</u>";
+    $vereniging = "<u>Koninklijke Vereniging De Friesche Elf Steden</u>";
+    $plaats = "<u>Leeuwarden</u>";
+    $provincie = "<u>Friesland</u>";
+    $hoevaak = "<u>15</u>";
     $jaar = "<u>1909</u>";
-    $verhaal = "De $elf (Fries: $alve) is een $km
-                kilometer lange $schaats over $natuur die wordt
-                georganiseerd door de $kvdfes. $leeuw, de hoofdstad van $fries, is start- en
-                aankomstplaats. De $elf is inmiddels $num maal
+
+    $verhaal = "De $soorttocht (Fries: $tocht_fr) is een $km
+                kilometer lange $hoevaak over $type die wordt
+                georganiseerd door de $vereniging. $plaats, de hoofdstad van $provincie, is start- en
+                aankomstplaats. De $soorttocht is inmiddels $hoevaak maal
                 verreden en werd voor het eerst in $jaar gereden en wordt
                 maximaal 1 keer per winter gehouden.";
     echo ("<h1>Taak 1</h1>");
     echo ("<p>$verhaal</p>");
 
-    $verhaal2 = "De " . $elf .  " (Fries: " . $alve . ") is een " . $km . " kilometer lange " . $schaats . " over " . $natuur . " die wordt
-                georganiseerd door de " . $kvdfes .". " . $leeuw . ",  de hoofdstad van " . $fries . ",  is start- en
-                aankomstplaats. De " . $elf . " is inmiddels " . $num . " maal
+    $verhaal2 = "De " . $soorttocht .  " (Fries: " . $tocht_fr . ") is een " . $km . " kilometer lange " . $hoevaak . " over " . $type . " die wordt
+                georganiseerd door de " . $vereniging .". " . $plaats . ",  de hoofdstad van " . $provincie . ",  is start- en
+                aankomstplaats. De " . $soorttocht . " is inmiddels " . $hoevaak . " maal
                 verreden en werd voor het eerst in " . $jaar . " gereden en wordt
                 maximaal 1 keer per winter gehouden.";
     echo ("<h1>Taak 2</h1>");
