fdaf535b0eada9207a2a70bee272c7c08e999655
Milad <milad.izadkhast@edu-kw1c.nl>
2020-03-02 10:55:13 +0100

    Include toegevoegd

---
+++ Include/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+
+    </title>
+
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <meta charset="utf-8">
+    <link href="../CSS/style.css" type="text/css" rel="stylesheet">
+
+</head>
+<body>
+<h2>Uitwerking van <a href="../Index.php"> PHP-opdrachten</a></h2>
+<div class="box">
